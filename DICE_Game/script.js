7208bae57a2e1c7e71b5c0b848de90ba40941f21
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-08-09 20:48:49 +0545

    Simple Dice game

---
+++ DICE_Game/script.js
@@ -0,0 +1,38 @@
+function rollDice() {
+    var number = Math.floor(Math.random() * 6) + 1;
+    console.log(number);
+    document.getElementById("roll").value = number;
+    display(number);
+}
+
+function display(number) {
+    let children = document.getElementsByClassName("child");
+    for (let i = 0; i < children.length; i++) {
+        children[i].style.visibility = "hidden";
+    }
+    let toShow = [];
+    switch (number) {
+        case 1:
+            toShow = [5];
+            break;
+        case 2:
+            toShow = [1, 9];
+            break;
+        case 3:
+            toShow = [1, 5, 9];
+            break;
+        case 4:
+            toShow = [1, 3, 7, 9];
+            break;
+        case 5:
+            toShow = [1, 3, 5, 7, 9];
+            break;
+        case 6:
+            toShow = [1, 3, 4, 6, 7, 9];
+            break;
+    }
+
+    for (let i = 0; i < toShow.length; i++) {
+        document.getElementById("c" + toShow[i]).style.visibility = "visible";
+    }
+}
